9488dc1ad499ee239604984101864b642f8683e9
[Faiz Kasman] <[faizkasman97@gmail.com]>
2022-05-17 21:20:05 +0800

    added plugins,removed horizontal scroll bar + custom red scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Banner from '../components/Banner'
 import Header from '../components/Header'
 import requests from '../utils/requests'
 import { Movie } from '../typings'
+import Row from '../components/Row'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -29,8 +30,7 @@ const Home = ({
 } : Props) => {
 
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10
-    to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -42,13 +42,12 @@ const Home = ({
       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         {/* Banner */}
         <Banner netflixOriginals={netflixOriginals}/>
-        <section>
+        <section className='md:space-y-24'>
           {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          <Row title="Comedies" movies={comedyMovies} />
         </section>
       </main>
       {/* Modal */}
